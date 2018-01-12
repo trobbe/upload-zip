fc186e4cee4d7151963558551f39539217dfad3e
trobbe <thomas.robbe62@gmail.com>
2018-01-12 13:43:29 +0100

    last commit

--- uploadZip.php
+++ uploadZip.php
@@ -6,6 +6,44 @@
  * - check the content type
  * - check the upload max size limit
  */
+$images = [];
+switch ($_FILES['the_file']['error']) {
+    case UPLOAD_ERR_INI_SIZE:
+        $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+        break;
+    case UPLOAD_ERR_FORM_SIZE:
+        $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+        break;
+    case UPLOAD_ERR_PARTIAL:
+        $message = "The uploaded file was only partially uploaded";
+        break;
+    case UPLOAD_ERR_NO_FILE:
+        $message = "No file was uploaded";
+        break;
+    case UPLOAD_ERR_NO_TMP_DIR:
+        $message = "Missing a temporary folder";
+        break;
+    case UPLOAD_ERR_CANT_WRITE:
+        $message = "Failed to write file to disk";
+        break;
+    case UPLOAD_ERR_EXTENSION:
+        $message = "File upload stopped by extension";
+        break;
+    case UPLOAD_ERR_OK:
+//uploading successfully done
+        if (mime_content_type($_FILES['the_file']['tmp_name']) === 'application/zip') {
+            $images[]= $_FILES['the_file']['name'];
+            $target_path = "thumbnails/" .microtime(true).'.zip';
+            var_dump(move_uploaded_file($_FILES["the_file"]["tmp_name"], $target_path));
+        }
+        else {
+            $message = "The file isn't a zip";
+        }
+        break;
+
+    default:
+        $message = "Unknown upload error";
+}
 
 // if all went fine ===>
 
@@ -70,7 +108,41 @@
 // 22) check if there were at least 1 image in the zip
 
 // need to force apache header content type to json if the server always return "text/html" content type
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 
 // 23) finally return error message or the array of images
-echo json_encode($_FILES);
+
+/*if($_FILES["the_file"]["name"]) {
+    $filename = $_FILES["the_file"]["name"];
+    $source = $_FILES["the_file"]["tmp_name"];
+    $type = $_FILES["the_file"]["type"];
+
+    $name = explode(".", $filename);
+    $accepted_types = 'application/zip';
+    if($accepted_types == $type) {
+        $okay = true;
+    }
+}
+
+    $continue = strtolower($name[1]) == 'zip' ? true : false;
+    if(!$continue) {
+        $message = "The file you are trying to upload is not a .zip file. Please try again.";
+    }
+
+    $target_path = "/home/wali12/sites/upload-zip".$filename;  // change this to the correct site path
+    if(move_uploaded_file($source, $target_path)) {
+        $zip = new ZipArchive();
+        $x = $zip->open($target_path);
+        if ($x === true) {
+            $zip->extractTo("/home/var/yoursite/httpdocs/"); // change this to the correct site path
+            $zip->close();
+
+            unlink($target_path);
+        }
+        $message = "Your .zip file was uploaded and unpacked.";
+    } else {
+        $message = "There was a problem with the upload. Please try again.";
+    }
+}*/
+
+echo json_encode($message ?? $images);
